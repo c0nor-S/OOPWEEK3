f84ecf581d254d8d0011e6c234ac96dcedf9c190
c0nor-S <conor.shaughnessy04@gmail.com>
2025-09-25 17:51:48 +0100

    Added New Method Called 'Add'

---
+++ src/main/java/ie/atu/week3/Calculator.java
@@ -0,0 +1,9 @@
+package ie.atu.week3;
+
+public class Calculator {
+    public int add(int firstNum, int secondNum)
+    {
+        return firstNum + secondNum;
+    }
+    // You Will Add/Subtract/Multiply/Divide Later
+}
