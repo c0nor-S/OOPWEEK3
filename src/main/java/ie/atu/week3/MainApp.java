aadd6dae10460722b33239191b25efc8d6375635
c0nor-s <g00432599@atu.ie>
2025-09-29 10:33:09 +0100

    Added New Method Called 'Operation'

--- src/main/java/ie/atu/week3/MainApp.java
+++ src/main/java/ie/atu/week3/MainApp.java
@@ -3,36 +3,58 @@ package ie.atu.week3;
 import java.util.Scanner;
 
 public class MainApp {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Please Enter Your First Number: ");
         int firstNumber = scan.nextInt();
+
         System.out.println("Please Enter Your Second Number:");
         int secondNumber = scan.nextInt();
 
+        System.out.println("Please Enter An Operation: ");
+        String operation = scan.next();
+
         Calculator calc = new Calculator();
-        int addResult = calc.add(firstNumber, secondNumber);
-        int subResult = calc.sub(firstNumber, secondNumber);
-        int mulResult = calc.mul(firstNumber, secondNumber);
-        int powResult = calc.pow(firstNumber, secondNumber);
-        int modResult = calc.mod(firstNumber, secondNumber);
 
-        System.out.println("The Addition Result Is: " + addResult);
-        System.out.println("The Subtraction Result Is: " + subResult);
-        System.out.println("The Multiplication Result Is: " + mulResult);
 
-        if(secondNumber == 0){
-            System.out.println("Error: Divide By Zero Error!");
+        switch (operation) {
+            case "add":
+                int addResult = calc.add(firstNumber, secondNumber);
+                System.out.println("The Addition Result Is: " + addResult);
+            break;
+
+            case "subtract":
+                int subResult = calc.sub(firstNumber, secondNumber);
+                System.out.println("The Subtraction Result Is: " + subResult);
+            break;
+
+            case "multiply":
+                int mulResult = calc.mul(firstNumber, secondNumber);
+                System.out.println("The Multiplication Result Is: " + mulResult);
+            break;
+
+            case "divide":
+            if (secondNumber == 0) {
+                System.out.println("Error: Divide By Zero Error!");
+                break;
+            } else {
+                int divResult = calc.div(firstNumber, secondNumber);
+                System.out.println("The Division Result Is: " + divResult);
+            }
+            break;
+
+            case "power":
+                int powResult = calc.pow(firstNumber, secondNumber);
+                System.out.println("The Exponential Result Is: " + powResult);
+            break;
+
+            case "modulus":
+                int modResult = calc.mod(firstNumber, secondNumber);
+                System.out.println("The Modulus Result Is: " + modResult);
+            break;
+
+            default:
         }
-        else
-        {
-            int divResult = calc.div(firstNumber, secondNumber);
-            System.out.println("The Division Result Is: " + divResult);
-        }
-
-        System.out.println("The Exponential Result Is: " + powResult);
-        System.out.println("The Modulus Result Is: " + modResult);
     }
 }
